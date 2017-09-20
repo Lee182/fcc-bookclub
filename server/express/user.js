5c92c98738757adcf08dc7e8b897b2bc06932fc4
Jonathan T L Lee <jono-lee@hotmail.co.uk>
2017-09-20 19:14:42 +0100

    package.json

--- server/express/user.js
+++ server/express/user.js
@@ -1,46 +1,45 @@
-module.exports = function({app, dao, port, k, sessiondb_name}){
-
+module.exports = function ({app, dao, port, k, sessiondb_name}) {
   const tw = require('./twitter_session.js')({
     dao,
     port,
     coll_name: sessiondb_name,
     consumerKey: k.twitter.consumerKey,
     consumerSecret: k.twitter.consumerSecret,
-    callbackUrl: 'https://booktrade.blogjon.com/tw.login_cb'
+    callbackUrl: 'https://booktrade.blogjono.com/tw.login_cb'
   })
   app.get('/tw.login', tw.login)
-  app.get('/tw.login_cb', tw.login_cb, function(req,res,next){
+  app.get('/tw.login_cb', tw.login_cb, function (req, res, next) {
     console.log(req.twuser)
     if (req.twuser === undefined) {
       return res.redirect('/')
     }
     dao.user__findOne({user_id: req.twuser})
-      .then(function(user){
+      .then(function (user) {
         if (user === undefined) {
           return dao.user__add({user_id: req.twuser})
         }
         return user
       })
-      .then(function(user){
+      .then(function (user) {
         if (user.loci === undefined) {
           return dao.user__add_loci_fromip({user_id: user._id, ip: req.ip})
         }
         return user
       })
-      .then(function(user){
+      .then(function (user) {
         console.log(user)
         res.redirect(`/?user_id=${req.twuser}`)
       })
   })
   app.post('/tw.logout', tw.logout)
 
-  app.get('/user', tw.is_logged_in, function(req,res,next){
-    dao.user__findOne({user_id: req.twuser}).then(function(user){
+  app.get('/user', tw.is_logged_in, function (req, res, next) {
+    dao.user__findOne({user_id: req.twuser}).then(function (user) {
       return res.json(user)
     })
   })
-  app.post('/user/:user_id', function(req,res){
-    dao.user__findOne({user_id: req.params.user_id}).then(function(user){
+  app.post('/user/:user_id', function (req, res) {
+    dao.user__findOne({user_id: req.params.user_id}).then(function (user) {
       if (user === undefined) {
         res.json({err: 'notfound'})
       }
@@ -48,15 +47,14 @@ module.exports = function({app, dao, port, k, sessiondb_name}){
     })
   })
 
-  app.post('/user_loci__change', tw.is_logged_in, function(req,res,next){
+  app.post('/user_loci__change', tw.is_logged_in, function (req, res, next) {
     dao.user__change_loci({
       user_id: req.twuser,
       loci: req.body.loci
-    }).then(function(result){
+    }).then(function (result) {
       res.json(result)
     })
   })
 
   return tw
-
 }
